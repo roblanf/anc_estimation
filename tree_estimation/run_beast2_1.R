6826191504d27a8ff5d176de1da9621a2e116b78
sebastianduchene <sebastian.duchene@sydney.edu.au>
2014-11-10 17:15:14 +1100

    wow

--- tree_estimation/run_beast2_1.R
+++ tree_estimation/run_beast2_1.R
@@ -3,18 +3,18 @@ source('../R/functions.R')
 #~/Desktop/progs/beast2/bin/beast
 
 
-s_len = 500
-for(i in 1:100){
+s_len = 1000
+for(i in 1:10){
 
 sim_dat1 <- get_tree_Q(tree_age = 1, taxa = 50, rate = 0.1, n_trans = 10, seq_length = s_len)
 write.dna(sim_dat1$seq_data, file = 'sdat_temp_500.fasta', format = 'fasta', nbcol = -1, colsep = '')
 
-file_dat1 <- make_beast_xml(sim_dat1$seq_data, f_name = 'test_dat2', min_root = 0.9, max_root = 1.1)
-cat(file_dat1, file= 'test_dat2.xml', sep = '\n')
+file_dat1 <- make_beast_xml(sim_dat1$seq_data, f_name = paste0('test_dat', i), min_root = 0.9, max_root = 1.1)
+cat(file_dat1, file= paste0('test_dat', i, '.xml'), sep = '\n')
 
-out_test1 <- run_beast(xml_path = 'test_dat2.xml', beast2_path = '~/Desktop/progs/beast2/bin/beast -overwrite -beagle', tree_ann_path = '~/Desktop/progs/beast2/bin/treeannotator -burnin 10')
+out_test1 <- run_beast(xml_path = paste0('test_dat', i, '.xml'), beast2_path = '~/Desktop/progs/beast2/bin/beast -overwrite -beagle', tree_ann_path = '~/Desktop/progs/beast2/bin/treeannotator -burnin 10')
 #system('rm out_temp.tree')
-cat(paste(s_len, mean(out_test1$node_support), out_test1$dist_t_tre, sep = ' '), file = 'seq_length_test_500.txt', sep = '\n', append = T)
+cat(paste(s_len, mean(out_test1$node_support), out_test1$dist_t_tre, sep = ' '), file = 'seq_length_test_1000.txt', sep = '\n', append = T)
 
 }
 
