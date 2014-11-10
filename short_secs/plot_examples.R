2144948c2bbe3aad5ce9b6259b2d29dfb1dc38bc
sebastianduchene <sebastian.duchene@sydney.edu.au>
2014-11-11 08:07:22 +1100

    wow

---
+++ short_secs/plot_examples.R
@@ -0,0 +1,73 @@
+source('R/functions.R')
+
+# Put true tree here
+tr_true <- ladderize(read.tree('sim_tr7.tree'))
+tr_true$edge.length <- tr_true$edge.length / max(branching.times(tr_true))
+
+
+# Load estimated trees here
+est_chrono <- ladderize(read.nexus('ann_tr7.tree'))
+postres <- read.nexus('test_dat7.trees')
+postres <- lapply(1:length(postres), function(x) ladderize(postres[[x]]))
+class(postres) <- 'multiPhylo'
+
+#post_nodes <- round(rowSums(sapply((length(postres)-50):(length(postres)), function(x) prop.clades(est_chrono, postres[[x]]))) / 51, 2)
+print('annotating posterior')
+post_nodes <- round(rowSums(sapply((length(postres) - 50):(length(postres)), function(x) prop.clades(est_chrono, postres[[x]]) != 0  )) / 51, 2)
+
+
+dif <- 1
+while(dif < 2.5){
+
+tr_q <- get_Q(tr_true, 3)
+
+n_trans <- 0
+while( n_trans < 2){
+  tr_s <- sim.history(tr_true, tr_q)
+  n_trans <- length(unique(get_node_states(tr_s)))
+}
+
+####
+get_fitted_node_states <- function(reroot_tree){
+  rec <- reroot_tree$marginal.anc
+  marginal_anc <- colnames(rec)[sapply(1:nrow(rec), function(x) which.max(rec[x, ]))]
+  return(marginal_anc)  
+}
+####
+
+fit_true <- rerootingMethod(tr_true, tr_s$states, model = 'SYM')
+true_nodes <- get_fitted_node_states(fit_true)
+tips_true <- tr_s$states[match(tr_true$tip.label, names(tr_s$states))]
+fit_states_true <- get_fitted_states(fit_true, tr_s$states, tr_true)
+# NSTATES FOR TRUE HERE
+n_states_true <- sum(sapply(1:nrow(fit_states_true), function(x) fit_states_true[x, 1] != fit_states_true[x, 2]))
+
+
+fit_1 <- rerootingMethod(est_chrono, tr_s$states, model = 'SYM')
+fit_nodes <- get_fitted_node_states(fit_1)
+tip_states <- tr_s$states[match(est_chrono$tip.label, names(tr_s$states))] 
+fit_states_1 <- get_fitted_states(fit_1, tip_states, est_chrono)
+# NSTATES FOR MPE
+n_states_1 <- sum(sapply(1:nrow(fit_states_1), function(x) fit_states_1[x, 1] != fit_states_1[x, 2]))
+
+dif <- n_states_1 / n_states_true
+
+print( dif)
+}
+
+pdf('set1_7.pdf', useDingbats = F, width = 14, height = 7)
+par(mfrow = c(1, 2))
+plot(tr_true, show.tip.label = F)
+tiplabels(tips_true, cex = 0.5, frame = 'circle', bg = factor(tips_true, ordered = T), col = 'white')
+tiplabels(tr_true$tip.label, adj = c(-0.7, 0.5), cex = 0.5, frame = 'none')
+nodelabels(true_nodes, cex = 0.5, frame = 'circle', bg = factor(true_nodes, ordered = T), col = 'white')
+text(paste('T=', n_states_true), x= 0.2, y = 0, cex = 0.5)
+
+plot(est_chrono, show.tip.label = F)
+tiplabels(tip_states, cex = 0.5, frame = 'circle', bg = factor(tip_states, ordered = T), col = 'white')
+tiplabels(est_chrono$tip.label, adj = c(-0.7, 0.5), frame = 'none', cex = 0.5)
+nodelabels(fit_nodes, cex = 0.5, frame = 'circle', bg = factor(fit_nodes, ordered = T), col = 'white')
+nodelabels(post_nodes, adj = c(1.2, -1), frame = 'none', cex = 0.5)
+text(paste('MPS=', round(mean(post_nodes), 2), 'T=', n_states_1), x = 0.2, y = 0, cex = 0.5)
+
+dev.off()
---
+++ short_secs/run_beast2_1.R
@@ -0,0 +1,22 @@
+source('R/functions.R')
+
+#~/Desktop/progs/beast2/bin/beast
+
+
+s_len = 100
+for(i in 1:10){
+
+sim_dat1 <- get_tree_Q(tree_age = 1, taxa = 50, rate = 0.1, n_trans = 10, seq_length = s_len)
+write.dna(sim_dat1$seq_data, file = 'sdat_temp_500.fasta', format = 'fasta', nbcol = -1, colsep = '')
+write.tree(sim_dat1$chronogram, file = paste0('sim_tr', i, '.tree'))
+
+file_dat1 <- make_beast_xml(sim_dat1$seq_data, f_name = paste0('test_dat', i), min_root = 0.9, max_root = 1.1)
+cat(file_dat1, file= paste0('test_dat', i, '.xml'), sep = '\n')
+
+out_test1 <- run_beast(sim_dat1, xml_path = paste0('test_dat', i, '.xml'), beast2_path = '~/Desktop/progs/beast2/bin/beast -overwrite -beagle', tree_ann_path = '~/Desktop/progs/beast2/bin/treeannotator -burnin 10')
+
+system(paste0('mv out_temp.tree ', 'ann_tr', i, '.tree'))
+cat(paste(s_len, mean(out_test1$node_support), out_test1$dist_t_tre, sep = ' '), file = 'seq_length_test_1000.txt', sep = '\n', append = T)
+
+}
+
